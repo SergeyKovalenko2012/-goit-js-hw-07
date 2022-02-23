ae45e3d2821daeec3279e15415dea7485ced8788
SergeyKovalenko2012 <sergeykovalenko201290@gmail.com>
2022-02-23 21:18:57 +0200

    hw07.2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const marcupPicture = createPictureCard(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", marcupPicture);
+
+function createPictureCard(pictures) {
+  return pictures
+    .map(({ preview, description, original }) => {
+      return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`;
+    })
+    .join("");
+}
+
+const gallery = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionPosition: "botom",
+  captionsData: "alt",
+  captionSelector: "img"
+});
